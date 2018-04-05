c5469d7183af5aa448f018542ba490cc32b77c21
admin1 <MohamedMFouad>
2018-04-05 18:02:20 +0200

    parsing

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,21 @@
 var express =require ('express');
 var app = express();
+var bodyParser =require('body-parser');
 
 var message = [{text: "mmmm", owner: "mmm"},{kks: "sdasd" , dasdas : "mmsamdnmd"}  ]
 
+app.use(bodyParser.json());
 app.use((req, res, next)=>{
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, x-Requested-With, Content-Type, Accept");
     next();
 })
 
-app.get('/', (req, res)=>{
+app.get('/message', (req, res)=>{
     res.json(message);
 })
-
-app.listen(1234)
+app.post('/message', (req, res)=>{
+    res.json(message)
+    console.log(req.body)
+}) 
+app.listen(1234)    
